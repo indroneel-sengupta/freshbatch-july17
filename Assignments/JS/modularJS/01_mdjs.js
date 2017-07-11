61f58508d02ce643ea61f2766c8e1dea0e10fb75
indroneel-sengupta <neelsengupta2795@gmail.com>
2017-07-11 13:56:28 +0530

    commit

--- Assignments/JS/modularJS/01_mdjs.js
+++ Assignments/JS/modularJS/01_mdjs.js
@@ -53,3 +53,57 @@ function test3(){
 	keg.deposit(35);
 	keg.withdraw(35);
 }
+
+function Keg1(accno,accbal,accname) {
+/* ... private members ... */
+	var accNo = accno;
+	var accBal = accbal;
+	var accName = accname;
+	var obj = {
+		fill: function(acnum,acbal,acname) { 
+			accNo = acnum;
+			accBal = acbal;
+			accName = acname;
+		},
+		withdraw: function(amt) {
+			accBal = parseInt(accBal) - parseInt(amt);
+			document.writeln("balance for "+accName+" is "+accBal+"</br>");
+	
+		},
+		deposit: function(amt) {
+			accBal = parseInt(accBal) + parseInt(amt);
+			document.writeln("balance for "+accName+" is "+accBal+"</br>");
+		}
+	}
+	return obj;
+}
+function test4(){
+	var keg = new Keg1("99999", "11111","Test4");
+	keg.withdraw(102); // 100.0
+	keg.deposit(102);
+	
+}
+
+function Keg2(accNo,accBal,accName){
+	this.accNo = accNo;
+	this.accBal = accBal;
+	this.accName = accName;
+}
+
+Keg2.prototype.withdraw = function(amt){
+	accBal = parseInt(this.accBal) - parseInt(amt);
+	document.writeln("balance for "+this.accName+" is "+accBal+"<br>");
+
+};
+
+Keg2.prototype.deposit = function(amt){
+	accBal = parseInt(this.accBal) + parseInt(amt);
+	document.writeln("balance for "+this.accName+" is "+accBal+"<br>");
+
+};
+
+function test5(){
+	var keg = new Keg2("55555","33333","Test 5");
+	keg.withdraw(201);
+	keg.deposit(101);
+}
