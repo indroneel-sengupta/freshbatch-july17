4861865db9b1724b190b1877ce567cbf71396cfd
indroneel-sengupta <neelsengupta2795@gmail.com>
2017-08-28 17:43:38 +0530

    latest

---
+++ Assignments/dotNet/BackupService/BackupService/Service1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BackupService
+{
+    public partial class BackupService : ServiceBase
+    {
+        string mainPath = @"D:\project_dir\freshbatch-july17\Assignments\dotNet\Main Path\";
+        string backupPath = @"D:\project_dir\freshbatch-july17\Assignments\dotNet\Backup Path\";
+        
+
+        public BackupService()
+        {
+            InitializeComponent();
+            if (!System.Diagnostics.EventLog.SourceExists("BackupSvcSource"))
+                System.Diagnostics.EventLog.CreateEventSource("BackupSvcSource", "BackupSvcLog");
+            eventLog1.Source = "BackupSvcSource";
+            eventLog1.Log = "BackupSvcLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher(mainPath);
+            eventLog1.WriteEntry("Monitoring Main Path");
+            watcher.Created += Watcher_Created;
+            watcher.Changed += Watcher_Changed;
+            watcher.Deleted += Watcher_Deleted;
+            watcher.Renamed += Watcher_Renamed;
+            watcher.Error += Watcher_Error;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void Watcher_Error(object sender, ErrorEventArgs e)
+        {
+            eventLog1.WriteEntry("Errors found : ");
+        }
+
+        private void Watcher_Created(object sender, FileSystemEventArgs e)
+        {
+
+            eventLog1.WriteEntry("File Created");
+            if (File.Exists(mainPath + e.Name))
+                  File.Copy(mainPath + e.Name, backupPath + e.Name, true);
+        }
+
+        private void Watcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            eventLog1.WriteEntry("File Renamed");
+            if (File.Exists(mainPath + e.Name))
+            {
+                File.Copy(mainPath + e.Name, backupPath + e.Name, true);
+                File.Delete(backupPath + e.OldName);
+            }
+            
+        }
+
+        private void Watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            eventLog1.WriteEntry("File Deleted");
+           
+        }
+
+        private void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            eventLog1.WriteEntry("File Changed");
+            if (File.Exists(mainPath + e.Name))
+                File.Copy(mainPath + e.Name,backupPath +e.Name,true);
+        }
+
+        protected override void OnStop()
+        {
+        }
+        protected override void OnPause()
+        {
+            base.OnPause();
+        }
+
+        protected override void OnContinue()
+        {
+            base.OnContinue();
+        }
+
+    }
+}
---
+++ Assignments/dotNet/WindowsServiceDemo/WindowsServiceDemo/Service1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsServiceDemo
+{
+    public partial class WindowsServiceDemo : ServiceBase
+    {
+        public WindowsServiceDemo()
+        {
+            InitializeComponent();
+            if (!System.Diagnostics.EventLog.SourceExists("MyPracSvcSource"))
+                System.Diagnostics.EventLog.CreateEventSource("MyPracSvcSource","MyPracSvcLog");
+            eventLog1.Source = "MyPracSvcSource";
+            eventLog1.Log = "MyPracSvcLog";
+         }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("My service started for Demo");
+
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 60000;//60 seconds
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        private void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            eventLog1.WriteEntry("Monitoring the service",EventLogEntryType.Information);
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("My service stopped for Demo");
+
+        }
+    }
+}
